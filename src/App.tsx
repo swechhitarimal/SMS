5a14dd23d2a68038674f61bafa81928485f97a30
Swechhita <swechhitarimal10@gmail.com>
2025-07-16 12:27:16 +0545

    dashboard change

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+//import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -7,13 +9,14 @@ import Inventory from "./pages/Inventory";
 import Sales from "./pages/Sales";
 import Customers from "./pages/Customers";
 import Analytics from "./pages/Analytics";
-import { Toaster } from "sonner";
+//import NotFound from "./pages/NotFound";
+
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      
       <Sonner />
       <BrowserRouter>
         <Routes>
@@ -23,7 +26,7 @@ const App = () => (
           <Route path="/customers" element={<Customers />} />
           <Route path="/analytics" element={<Analytics />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          {/* <Route path="*" element={<NotFound />} /> */}
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
